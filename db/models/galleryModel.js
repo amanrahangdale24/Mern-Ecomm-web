1ded61cd0b90e137c6be9ec0b4e8bc885867aa92
Aman Rahangdale <147967516+amanrahangdale24@users.noreply.github.com>
2024-12-07 22:00:27 +0530

    Remove files from tracking to apply .gitignore

--- db/models/galleryModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const gallerySchema = mongoose.Schema(
-    { image: { type: String } },
-    { timestamps: true } 
-);
-
-module.exports = mongoose.model('Gallery', gallerySchema); // is mongodb, this collection is converted into galleries. 
